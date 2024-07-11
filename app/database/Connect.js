59f751bb7b3700fa852000b16d3210ea2933ccc1
shubham kumar <theshubham.dev@gmail.com>
2024-07-12 00:32:53 +0530

    fix

--- app/database/Connect.js
+++ app/database/Connect.js
@@ -3,9 +3,12 @@ import { NextResponse } from "next/server";
 
 async function Connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://cypressadmin:cypresspswd@cluster0.k8uxneb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/hotelblogwebapp"
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
     console.log("database connected");
   } catch (e) {
     return NextResponse.json({ msg: e.message });
