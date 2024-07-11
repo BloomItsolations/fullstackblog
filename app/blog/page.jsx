ac681f7095fed0cb325774fc017135283911607a
shubham kumar <theshubham.dev@gmail.com>
2024-07-12 00:39:27 +0530

    fix

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,15 +1,12 @@
 import styles from "./blog.module.css";
-import Menu from "../component/menu/Menu";
 import CardList from "../component/cardList/CardList";
 
 const BlogPage = () => {
-  
   return (
     <div className={styles.container}>
       <h1 className={styles.title}> Blog</h1>
       <div className={styles.content}>
         <CardList />
-        <Menu />
       </div>
     </div>
   );
