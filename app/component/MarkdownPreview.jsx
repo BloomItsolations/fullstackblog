225959a9714ed6d0d7dff224376fdb071684ac75
shubham kumar <theshubham.dev@gmail.com>
2024-07-12 17:53:25 +0530

    fix

--- app/component/MarkdownPreview.jsx
+++ app/component/MarkdownPreview.jsx
@@ -1,13 +1,3 @@
-import { Remarkable } from "remarkable";
-
-const md = new Remarkable();
-
-function renderMarkdownToHTML(markdown) {
-  const renderedHTML = md.render(markdown);
-  return { __html: renderedHTML };
-}
-
 export default function MarkdownPreview({ markdown }) {
-  const markup = renderMarkdownToHTML(markdown);
-  return <div dangerouslySetInnerHTML={markup} />;
+  return <div dangerouslySetInnerHTML={{ __html: markdown }} />;
 }
