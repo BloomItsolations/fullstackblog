89cd490f852cd480b6c69ff9eccb65ac212947e3
shubham kumar <theshubham.dev@gmail.com>
2024-07-12 12:30:48 +0530

    fix

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -13,31 +13,31 @@ const SinglePage = async ({ params }) => {
   const isVideo = (url) => {
     return /\.(mp4|webm|ogg)$/i.test(url);
   };
-   
-  if(!data){
-    return <div>Loading ...</div>
+
+  if (!data) {
+    return <div>Loading ...</div>;
   }
 
   return (
     <div className={style.container}>
-      {
-        isVideo(newdata.image) ? <video
-          style={{ width: '1000px', height: '500px', objectFit: 'contain' }}
+      {isVideo(newdata.image) ? (
+        <video
+          style={{ width: "1000px", height: "500px", objectFit: "contain" }}
           controls
           className={style.media}
         >
           <source src={newdata.image} type="video/mp4" />
           Your browser does not support the video tag.
-        </video> : <Image
+        </video>
+      ) : (
+        <Image
           src={newdata.image}
           alt={newdata.title}
           width={1200}
           height={350}
           className={style.image}
         />
-
-      }
-
+      )}
 
       <div className={style.content}>
         <h1 className={style.title}>{newdata.title}</h1>
--- app/component/card/Card.jsx
+++ app/component/card/Card.jsx
@@ -1,68 +1,69 @@
-
-'use client'
-import React, { useState, useEffect } from 'react';
-import style from './card.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect } from "react";
+import style from "./card.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = ({ value }) => {
-    const [clientContent, setClientContent] = useState("");
+  const [clientContent, setClientContent] = useState("");
 
-    useEffect(() => {
-        if (value?.content) {
-            setClientContent(truncateText(value.content, 190));
-        }
-    }, [value]);
+  useEffect(() => {
+    if (value?.content) {
+      setClientContent(truncateText(value.content, 190));
+    }
+  }, [value]);
 
-    const truncateText = (text, maxLength) => {
-        if (text.length > maxLength) {
-            return text.substring(0, maxLength) + '...';
-        }
-        return text;
-    };
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+    return text;
+  };
 
-    const isVideo = (url) => {
-        return /\.(mp4|webm|ogg)$/i.test(url);
-    };
+  const isVideo = (url) => {
+    return /\.(mp4|webm|ogg)$/i.test(url);
+  };
 
-    return (
-        <div className={style.container} style={{ backgroundColor: "#f8f9f9" }}>
-            <div className={style.imageContainer}>
-                {isVideo(value.image) ? (
-                    <video
-                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover' }}
-                        controls
-                        className={style.image}
-                    >
-                        <source src={value.image} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                ) : (
-                    <Image
-                        src={value.image}
-                        alt=''
-                        fill
-                        className={style.image}
-                    />
-                )}
-            </div>
-            <div className={style.textContainer}>
-                <Link href={`/blog/${value?._id}`} className={style.title}>
-                    {truncateText(value?.title, 150)}
-                </Link>
-                <div
-                    className={style.desc}
-                    dangerouslySetInnerHTML={{ __html: clientContent }}
-                ></div>
-                <div className={style.cardbottombox}>
-                    <div>
-                        <Link href={`/blog/${value._id}`} className={style.link}>Read More..</Link>
-                    </div>
-                    <div>
-                        <p className={style.auther}>Author: {value?.authername}</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={style.container} style={{ backgroundColor: "#f8f9f9" }}>
+      <div className={style.imageContainer}>
+        {isVideo(value.image) ? (
+          <video
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+            controls
+            className={style.image}
+          >
+            <source src={value.image} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : (
+          <Image src={value.image} alt="" fill className={style.image} />
+        )}
+      </div>
+      <div className={style.textContainer}>
+        <Link href={`/blog/${value?._id}`} className={style.title}>
+          {truncateText(value?.title, 150)}
+        </Link>
+        <div
+          className={style.desc}
+          dangerouslySetInnerHTML={{ __html: clientContent }}
+        ></div>
+        <div className={style.cardbottombox}>
+          <div>
+            <Link href={`/blog/${value._id}`} className={style.link}>
+              Read More..
+            </Link>
+          </div>
+          <div>
+            <p className={style.auther}>Author: {value?.authername}</p>
+          </div>
         </div>
       </div>
     </div>
